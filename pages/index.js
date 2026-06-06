f4460fd9885b9ca33d199ecb163017373d715578
Igor Lucena Gomes <gomesworsnoop@gmail.com>
2026-06-06 20:04:52 +0000

    adicionando a pasta pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Estou fazendo isso por você, vou fazer algo muito importante pra gente. Te amo!</h1>
+    return <h1>Amanda, se você me ama, dê uma risadinha!</h1>
 }
 
 export default Home;
